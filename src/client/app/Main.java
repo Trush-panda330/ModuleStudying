35ddf96f2184504ef4406fe85d50543e95bc0f0c
Trash-panda330 <uetak330@gmail.com>
2024-07-28 22:33:45 +0900

    moduleのお勉強

---
+++ src/client/app/Main.java
@@ -0,0 +1,12 @@
+package app;
+
+import xlib.XTest;
+//import ylib.yTest;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Module System!");
+        XTest.x();
+//        YTest.y();
+    }
+}
